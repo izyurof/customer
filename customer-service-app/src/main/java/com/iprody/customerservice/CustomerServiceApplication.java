2d07dc69a6062431c7f6c6a754562ce01240a6b1
Andry Izyurov <149159419+izyurof@users.noreply.github.com>
2024-07-23 19:34:05 +0300

    move checkstyle to root of project (#16)

--- customer-service-app/src/main/java/com/iprody/customerservice/CustomerServiceApplication.java
+++ customer-service-app/src/main/java/com/iprody/customerservice/CustomerServiceApplication.java
@@ -3,6 +3,11 @@ package com.iprody.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the Customer Service application.
+ * Initializes and runs the Spring Boot application using SpringApplication.
+ */
+
 @SpringBootApplication
 public class CustomerServiceApplication {
 
