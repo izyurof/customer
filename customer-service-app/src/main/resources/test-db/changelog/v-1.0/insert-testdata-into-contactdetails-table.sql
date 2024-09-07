c6c8ec6180a02df6f10fe3e0ca3fb445253b2bb3
Andry Izyurov <149159419+izyurof@users.noreply.github.com>
2024-09-07 13:43:38 +0300

    added test data for e2e tests (#48)

---
+++ customer-service-app/src/main/resources/test-db/changelog/v-1.0/insert-testdata-into-contactdetails-table.sql
@@ -0,0 +1,6 @@
+INSERT INTO contactdetails (email, telegram_id)
+VALUES ('test@gmail.com', '@test_telegram_id'),
+       ('test2@gmail.com', '@test_telegram_id2'),
+       ('test3@gmail.com', '@test_telegram_id3'),
+       ('test4@gmail.com', '@test_telegram_id4'),
+       ('test5@gmail.com', '@test_telegram_id5');
---
+++ customer-service-app/src/main/resources/test-db/changelog/v-1.0/insert-testdata-into-customer-table.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers (name, surname, country_code_id, contact_details_id)
+VALUES ('test_name', 'test_surname', 1, 1),
+       ('test_name2', 'test_surname2', 2, 2),
+       ('test_name3', 'test_surname3', 3, 3),
+       ('test_name4', 'test_surname4', 4, 4),
+       ('test_name5', 'test_surname5', 5, 5);
